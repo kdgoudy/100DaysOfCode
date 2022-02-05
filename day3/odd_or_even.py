fb7b1e0aaad6b6e8a7336c436b36574c6a0edda0
Koki <kgoudy64@gmail.com>
2022-02-05 13:44:29 -0600

    Create odd_or_even.py

---
+++ day3/odd_or_even.py
@@ -0,0 +1,8 @@
+#Creating a variable to check numbers
+number = int(input("Which number do you want to check?: "))
+
+#if statment to check if number is even or odd
+if number % 2 == 0:
+    print("This is an even number.")
+else:
+    print("This is an odd number.")
